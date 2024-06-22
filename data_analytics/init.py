0c86bae8c8585cb6eaf47f2e2398b89c2237060e
KOSASIH <kosasihg88@gmail.com>
2024-06-22 17:35:03 +0700

    Delete data_analytics/init.py

--- data_analytics/init.py
+++
@@ -1,6 +0,0 @@
-# __init__.py
-from .data_ingestion import DataIngestion
-from .data_processing import DataProcessing
-from .data_visualization import DataVisualization
-
-__all__ = ['DataIngestion', 'DataProcessing', 'DataVisualization']
