fd135c3e5670a273c2bda982aabfad929e6372bd
KOSASIH <kosasihg88@gmail.com>
2024-06-22 17:34:19 +0700

    Create __init__.py

---
+++ data_analytics/__init__.py
@@ -0,0 +1,6 @@
+# __init__.py
+from .data_ingestion import DataIngestion
+from .data_processing import DataProcessing
+from .data_visualization import DataVisualization
+
+__all__ = ['DataIngestion', 'DataProcessing', 'DataVisualization']
